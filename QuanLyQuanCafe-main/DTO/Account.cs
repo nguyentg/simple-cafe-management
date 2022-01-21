e0f94cfa9b441756e5a12307ec8c0d890095ebff
Trần Gia Nguyên <75612224+nguyentg@users.noreply.github.com>
2022-01-21 08:42:52 +0700

    Add files via upload

---
+++ QuanLyQuanCafe-main/DTO/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DTO
+{
+    public class Account
+    {
+        private int id_staff;
+        public Account(string userName, string displayName, int type, string password = null, int id_staff = 0)
+        {
+            this.UserName = userName;
+            this.DisplayName = displayName;
+            this.Type = type;
+            this.Password = password;
+            this.id_staff = id_staff;
+        }
+
+        public Account(DataRow row)
+        {
+            this.UserName = row["userName"].ToString();
+            this.DisplayName = row["displayName"].ToString();
+            this.Type = (int)row["type"];
+            this.Password = row["password"].ToString();
+            this.Id_staff = (int)row["id_staff"];
+        }
+
+        private int type;
+
+        public int Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        private string displayName;
+
+        public string DisplayName
+        {
+            get { return displayName; }
+            set { displayName = value; }
+        }
+
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public int Id_staff { get => id_staff; set => id_staff = value; }
+    }
+}
---
+++ QuanLyQuanCafe-main/DTO/Staff.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DTO
+{
+    class Staff
+    {
+        private int id;
+        private string name;
+        private string sdt;
+        private string address;
+        private string sex;
+        private int salary;
+        private int status;
+        public Staff(DataRow row)
+        {
+            this.Id = (int)row["id"];
+            this.Name = row["name"].ToString();
+            this.Sdt = row["sdt"].ToString();
+            this.Address = row["Address"].ToString();
+            this.Sex = row["Sex"].ToString();
+            this.Salary = (int)row["Salary"];
+            this.Status = (int)row["Status"];
+
+        }
+
+        public Staff(int id, string name, string sdt, string address, string sex, int salary, int status)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Sdt = sdt;
+            this.Address = address;
+            this.Sex = sex;
+            this.Salary = salary;
+            this.Status = status;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Sdt { get => sdt; set => sdt = value; }
+        public string Address { get => address; set => address = value; }
+        public string Sex { get => sex; set => sex = value; }
+        public int Salary { get => salary; set => salary = value; }
+        public int Status { get => status; set => status = value; }
+    }
+}
